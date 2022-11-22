033ac52863d9b3a57d557cc2baca11b50dec56b6
sonorah <shalili71@smith.edu>
2022-11-22 18:57:00 -0500

    Updated parser.py so it deletes the intermediate csv file before writing a new one
    to fix issues with overwriting intermediate csvs

--- src/parser.py
+++ src/parser.py
@@ -1,17 +1,22 @@
 import xml.etree.ElementTree as ET
 import csv
+import os
+import os.path
 
 def parsexml(xml_file):
     tree = ET.parse(xml_file)
     root = tree.getroot()
-    path = "../misc/"
+    file = "../misc/reqs.csv"
+
+    #delete reqs file if it already exists
+    if os.path.isfile(file):
+        os.remove(file)
     # CREATE CSV FILE
-    csvfile = open(path + xml_file.split(".")[0] + ".csv",'w',encoding='utf-8')
+    csvfile = open(file,'w', encoding='utf-8')
     csvfile_writer = csv.writer(csvfile)
 
     # ADD THE HEADER TO CSV FILE
     csvfile_writer.writerow(["requirements"])
- 
     parsed_reqs = []
     for req in root.findall('.//{req_document.xsd}req/{req_document.xsd}text_body'):
         req_txt = req.text
@@ -22,5 +27,6 @@ def parsexml(xml_file):
             # ADD A NEW ROW TO CSV FILE
             csvfile_writer.writerow(csv_line)
     csvfile.close() 
+    
+    return file
 
-    return path + xml_file.split(".")[0] + ".csv"
