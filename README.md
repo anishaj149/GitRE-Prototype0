6fcb7cf864c5e6b9ac4271acf3e009924016c59a
A. M. Grubb <amgrubb@smith.edu>
2022-11-22 14:48:15 -0500

    Add scripts and oracle files.

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ Protype 0 for importing and parsing natural language requirements and creating t
 1. src - matcher and parser 
 2. data - git artifacts and requirement repository
 3. output - matchings (matrices and lists)
+4. precision-recall-calculations - calculating data for Table 2 in paper.
 
-## Instructions
+## Instructions for Run Matching
 1. Select one of three pairs of issues/requirements (keepass, peering, gammaj) from the `data` folder
 2. Run the driver file in the `src` folder through the terminal: `python3 driver.py <requirements-filename> <artifacts-filename>`
 3. Examine the matches in the `matchesList.csv` and the match matrix in `matchingsMatrx.csv` both located in the `output` folder
+
---
+++ precision-recall-calculations/calculate-results.py
@@ -0,0 +1,69 @@
+import csv
+
+## This python script calculates the precision and recall for 
+##  GitRE traceability links.
+
+#Data must appear in the .csv file in the follow form:
+#   req,issue
+#   1,34
+#   2,35
+
+def readDatabase(filename: str) -> list:
+    list = []
+    line_count = 0
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            if line_count == 0:
+                if not(row[0]=='req' and row[1]=='issue'):
+                    print("Columns not set up properly in " + filename)
+                    return [];            
+                # for element in row:
+                #     header.append(element)
+                line_count += 1
+            else:
+                list.append((row[0],row[1]))
+                line_count += 1
+    return list
+
+def calculatePrecisionAndRecall(oracle: list, testSet: list) -> None:
+    found = []      #true positives
+    missed = []     #false negatives
+    for item in oracle:
+        #Iterate over each element, do not edit the oracle.
+        success = False         #whether the item is found.
+        for test in testSet:
+            if item == test:
+                success = True
+                break
+        if success:
+            found.append(item)
+            testSet.remove(item)
+        else:
+            missed.append(item) 
+    truePos = len(found)
+    falseNeg = len(missed)
+    falsePos = len(testSet)
+    # print("True Positives:", truePos)
+    # print("False Negatives:", falseNeg)
+    # print("False Positives:", falsePos)
+    precision = round(truePos * 100 / (truePos + falsePos)) #, 0)
+    print("Precision:\t", precision)
+    recall = round(truePos * 100 / (truePos + falseNeg)) #, 0)
+    print("Recall:\t\t", recall)
+
+def runAllTests() -> None:
+    oracleFiles = ['keepass-oracle-set.csv', 'peering-oracle-set.csv', 'gammaj-oracle-set.csv']
+    testFiles = [['keepass-match1.csv','keepass-match2.csv','keepass-match3.csv'],
+                 ['peering-match1.csv','peering-match2.csv','peering-match3.csv'],
+                 ['gammaj-match1.csv','gammaj-match2.csv','gammaj-match3.csv']]
+    for i in range(len(oracleFiles)):
+        oracle = readDatabase(oracleFiles[i])
+        testFileList = testFiles[i]
+        for testName in testFileList:
+            testSet = readDatabase(testName)
+            print("Oracle:", oracleFiles[i], "Test File:", testName)
+            calculatePrecisionAndRecall(oracle, testSet)
+                
+if __name__ == "__main__":
+    runAllTests()
